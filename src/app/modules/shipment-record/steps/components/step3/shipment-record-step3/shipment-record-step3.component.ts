1e9ecd9f589ca5969229b9ff91fed42b225ec5f7
Alex <arojasvel@gmail.com>
2026-02-10 15:46:44 -0500

    add shipment summary item component in step 3

--- src/app/modules/shipment-record/steps/components/step3/shipment-record-step3/shipment-record-step3.component.ts
+++ src/app/modules/shipment-record/steps/components/step3/shipment-record-step3/shipment-record-step3.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { Button } from 'primeng/button';
-import { NgTemplateOutlet } from '@angular/common';
+
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { MarkupModal } from '@shared/components/modal/markup-modal';
+import { ShipmentSummaryItemComponent } from './components/shipment-summary-item/shipment-summary-item.component';
 
 @Component({
     selector: 'app-shipment-record-step3',
-    imports: [Button, NgTemplateOutlet],
+    imports: [Button, ShipmentSummaryItemComponent],
     templateUrl: './shipment-record-step3.component.html',
     styleUrl: './shipment-record-step3.component.scss',
     providers: [DialogService]
@@ -14,10 +15,76 @@ import { MarkupModal } from '@shared/components/modal/markup-modal';
 export class ShipmentRecordStep3Component {
     dialog = inject(DialogService);
 
+    shipments = [
+        {
+            id: 1,
+            city: 'Chiclayo',
+            customer: 'Angel Esquen',
+            description: 'Ropa y accesorios',
+            address: 'Av. Comandante Espinar',
+            total: 17.5
+        },
+        {
+            id: 2,
+            city: 'Chiclayo',
+            customer: 'Angel Esquen',
+            description: 'Ropa y accesorios',
+            address: 'Av. Comandante Espinar',
+            total: 17.5
+        },
+        {
+            id: 3,
+            city: 'Chiclayo',
+            customer: 'Angel Esquen',
+            description: 'Ropa y accesorios',
+            address: 'Av. Comandante Espinar',
+            total: 17.5
+        },
+        {
+            id: 4,
+            city: 'Chiclayo',
+            customer: 'Angel Esquen',
+            description: 'Ropa y accesorios',
+            address: 'Av. Comandante Espinar',
+            total: 17.5
+        },
+        {
+            id: 5,
+            city: 'Chiclayo',
+            customer: 'Angel Esquen',
+            description: 'Ropa y accesorios',
+            address: 'Av. Comandante Espinar',
+            total: 17.5
+        },
+        {
+            id: 6,
+            city: 'Chiclayo',
+            customer: 'Angel Esquen',
+            description: 'Ropa y accesorios',
+            address: 'Av. Comandante Espinar',
+            total: 17.5
+        },
+        {
+            id: 7,
+            city: 'Chiclayo',
+            customer: 'Angel Esquen',
+            description: 'Ropa y accesorios',
+            address: 'Av. Comandante Espinar',
+            total: 17.5
+        },
+        {
+            id: 8,
+            city: 'Chiclayo',
+            customer: 'Angel Esquen',
+            description: 'Ropa y accesorios',
+            address: 'Av. Comandante Espinar',
+            total: 17.5
+        }
+    ];
+
     ref: DynamicDialogRef | null = null;
     openModal() {
         this.ref = this.dialog.open(MarkupModal, {
-            header: 'Markup Modal',
             width: '371px',
             contentStyle: { 'max-height': '500px', overflow: 'auto' },
             // baseZIndex: 10000,
